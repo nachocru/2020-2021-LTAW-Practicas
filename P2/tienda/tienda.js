4a5c3c05122d80a3a85d6f99e57f5ef5786b4f5a
nachocru <i.cruzd.2017@alumnos.urjc.es>
2021-05-03 19:11:02 +0200

    Añadidas todas las fotos y contenido de las págimas de forma dinámica

---
+++ P2/tienda/tienda.js
@@ -0,0 +1,68 @@
+//-- Dependencias 
+const http = require('http');
+const fs = require('fs');
+const PORT = 9000
+
+const server = http.createServer((req, res) => {
+
+    console.log("Petición recibida!");
+    const myURL = new URL(req.url, 'http://' + req.headers['host']);
+    console.log("URL solicitada: " + myURL.pathname);
+
+    let page = '' //-- Página que queremos cargar
+    if (myURL.pathname == "/") { //-- Cuando lanzamos nuestra página web
+        page = './home.html'
+    } else { // -- En cualquier otro caso
+        page = '.' + myURL.pathname;
+    }
+
+    console.log("Página solicitada: " + page)
+
+    content = page.split(".").pop()
+    console.log("Contenido de la página: " + content);
+
+    if (page == './home.html') {
+        const HOME_HTML = fs.readFileSync('home.html', 'utf-8');
+        let content_type = "text/html";
+        let content = HOME_HTML.replace("PRODUCT1", "HOLA");
+        //-- Enviar la respuesta
+        res.setHeader('Content-Type', content_type);
+        res.write(content);
+        res.end()
+    } else {
+        fs.readFile(page, (err, data) => {
+            let code = 200;
+            let code_msg = "OK";
+            let content_type = "text/html";
+
+            if (err) {
+                console.log("Error!!")
+                console.log(err.message);
+                code = 404;
+                code_msg = "404 Not Found"
+                res.writeHead(code, { 'Content-Type': 'text/html' });
+                return res.end(code_msg);
+            } else {
+                switch (content) {
+                    case 'html':
+                        content_type = "text/html";
+                    case 'css':
+                        content_type = "text/css";
+                    default:
+                        content_type = content;
+                }
+                res.statusCode = code;
+                res.statusMessage = code_msg;
+                res.setHeader('Content-Type', content_type);
+                res.write(data);
+                res.end();
+            }
+
+        });
+    }
+
+});
+
+server.listen(PORT);
+
+console.log("Mi tienda. Escuchando en puerto: " + PORT);
