6015bc8f5d22d832bc1c6cd0f667c1faaa993672
nachocru <i.cruzd.2017@alumnos.urjc.es>
2021-05-28 23:08:17 +0200

    Lógica usuarios de chat

--- P3/chat/chat-server.js
+++ P3/chat/chat-server.js
@@ -3,10 +3,11 @@ const socket = require('socket.io');
 const http = require('http');
 const express = require('express');
 const colors = require('colors');
+const fs = require('fs');
 
 var users = 0;
 
-const PUERTO = 9002;
+const PUERTO = 9000;
 
 //-- Crear una nueva aplciacion web
 const app = express();
@@ -20,7 +21,13 @@ const io = socket(server);
 //-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
 //-- Definir el punto de entrada principal de mi aplicación web
 app.get('/', (req, res) => {
-    res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/chat.html">Test</a></p>');
+    res.send(fs.readFileSync('./public/home.html', 'utf-8'));
+    // res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/chat.html">Test</a></p>');
+});
+
+app.get('/procesar', (req, res) => {
+    res.send(fs.readFileSync('./public/chat.html', 'utf-8'));
+    // res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/chat.html">Test</a></p>');
 });
 
 //-- Esto es necesario para que el servidor le envíe al cliente la
@@ -37,6 +44,7 @@ io.on('connect', (socket) => {
     users += 1;
 
     console.log('** NUEVA CONEXIÓN **'.yellow);
+    io.send('Nuevo usuario conectado');
     socket.send('Bienvenid@ al chat de Nacho :)');
     //-- Evento de desconexión
     socket.on('disconnect', function() {
@@ -47,8 +55,8 @@ io.on('connect', (socket) => {
 
     //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
     socket.on("message", (msg) => {
-        if (msg.startsWith('/')) {
-            switch (msg) {
+        if (msg.split(': ')[1].startsWith('/')) {
+            switch (msg.split(': ')[1]) {
                 case '/help':
                     console.log('estamos en help')
                     break;
--- P3/chat/public/chat-client.js
+++ P3/chat/public/chat-client.js
@@ -6,14 +6,18 @@ const msg_entry = document.getElementById("msg_entry");
 const socket = io();
 
 socket.on("message", (msg) => {
-    display.innerHTML += '<p style="color:blue">' + msg + '</p>';
+    let params = window.location.search;
+    let urlParams = new URLSearchParams(params);
+    let color = urlParams.get('color');
+    display.innerHTML += '<p style="color:' + color + '" class="messages">' + '> ' + msg + '</p>';
 });
 
 //-- Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
+    let params = window.location.search;
+    let urlParams = new URLSearchParams(params);
+    let name = urlParams.get('name');
     if (msg_entry.value)
-        socket.send(msg_entry.value);
-
-    //-- Borrar el mensaje actual
+        socket.send(name + ': ' + msg_entry.value);
     msg_entry.value = "";
 }
